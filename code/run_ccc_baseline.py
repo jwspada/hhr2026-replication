04425e407681e10b271a243fc6b7a898ee38cb84
Joseph Spada <jspada@stanford.edu>
2026-01-26 15:29:47 -0800

    fixing folder structure

---
+++ code/run_ccc_baseline.py
@@ -0,0 +1,47 @@
+import sys
+from pathlib import Path
+
+# set working directory
+REPO_ROOT = Path(__file__).resolve().parents[1]
+
+# specify which ccc model to import
+sys.path.insert(0, str(REPO_ROOT / "code" / "ccc_models" / "baseline"))
+
+import ccc
+print("Baseline CCC:", ccc.__file__)
+
+from ccc.data import Assets
+from ccc.parameters import Specification, DepreciationParams
+from ccc.calculator import Calculator
+
+def calc_ucc(years):
+    # create instance of asset class
+    assets = Assets()
+
+    # set depreciation parameters
+    dp = DepreciationParams()
+
+    for year in years:
+        print("Calculating UCC for " + str(year))
+        # set default paramaters for current year
+        params = Specification(year = year, call_tc=False)
+        # calculate ucc by asset for current year
+        calc = Calculator(params, dp, assets)
+        
+        ucc_asset = calc.calc_by_asset()
+        # set filename
+        filename = (
+            REPO_ROOT
+            / "data"
+            / "ucc"
+            / "main"
+            / f"asset_ucc_{year}.csv"
+        )
+        #filename = PATH + "data/ucc/asset_ucc_qbi_" + str(year) + ".csv"
+        #filename = PATH + "data/ucc/asset_ucc_" + str(year) + "_tauonly.csv"
+        #filename = PATH + "data/ucc/asset_ucc_qbi_" + str(year) + "_tauonly.csv"
+        #filename = PATH + "data/ucc/asset_ucc_" + str(year) + "_bonusonly.csv"
+        # export to csv
+        ucc_asset.to_csv(filename, index = False, header = True)
+
+calc_ucc(range(1987,2024))
